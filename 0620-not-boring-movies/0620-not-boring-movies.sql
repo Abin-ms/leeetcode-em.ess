1fabf7db3f1c58cec49cab89118d2cbed85f1c3d
Abin M.S <abinn.mss@gmail.com>
2026-08-31 22:57:11 +0530

    Time: 358 ms (13.6%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 SELECT *
 FROM Cinema
-WHERE MOD(id, 2) != 0 AND description != 'boring' 
+WHERE ID % 2 != 0 AND description != 'boring' 
 ORDER BY rating desc;
