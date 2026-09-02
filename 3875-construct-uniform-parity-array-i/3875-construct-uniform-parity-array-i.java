6d88e2b1f840576607a95ff103bb7fadfce74d39
Abin M.S <abinn.mss@gmail.com>
2026-09-02 23:54:41 +0530

    Time: 1 ms (31.89%), Space: 44.9 MB (94.09%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,28 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+
+        int oddNumber = -1;
+
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                oddNumber = num;
+                break;
+            }
+        }
+
+        
+        if (oddNumber == -1) {
+            return true;
+        }
+
+        
+        for (int i = 0; i < nums1.length; i++) {
+
+            if (nums1[i] % 2 == 0) {
+                nums1[i] = nums1[i] - oddNumber;
+            }
+        }
+
         return true;
     }
 }
